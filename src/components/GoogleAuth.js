e1dc1d801260268bae16a1863a69f45277c1d924
Rodrigo Okuta <rodrigo.okuta@outlook.com>
2019-04-20 11:43:06 -0400

    connect google auth to redux

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { signIn, signOut } from '../actions'
 
 class GoogleAuth extends Component {
-
-    state = {
-        isSignedIn: null
-    }
 
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
@@ -13,14 +11,17 @@ class GoogleAuth extends Component {
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance()
-                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.onAuthChange(this.auth.isSignedIn.get())
                 this.auth.isSignedIn.listen(this.onAuthChange)
             })
         })
     }
 
-    onAuthChange = () => {
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    onAuthChange = isSignedIn => {
+        if (isSignedIn)
+            this.props.signIn()
+        else
+            this.props.signOut()
     }
 
     onSignInClick = () => {
@@ -32,10 +33,10 @@ class GoogleAuth extends Component {
     }
 
     renderAuthButton() {
-        if (this.state.isSignedIn === null)
+        if (this.props.isSignIn === null)
             return null
 
-        else if (this.state.isSignedIn)
+        else if (this.props.isSignIn)
             return (
                 <button className="ui red google button" onClick={this.onSignOutClick}>
                     <i className="google icon" />
@@ -61,4 +62,10 @@ class GoogleAuth extends Component {
     }
 }
 
-export default GoogleAuth;
+const mapStateToProps = state => {
+    return {
+        isSignIn: state.auth.isSignIn
+    }
+}
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth)
---
+++ src/reducers/authReducer.js
@@ -0,0 +1,15 @@
+const INITIAL_STATE = {
+    isSignIn: null
+}
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'SIGN_IN':
+            debugger
+            return { ...state, isSignIn: true }
+        case 'SIGN_OUT':
+            return { ...state, isSignIn: false }
+        default:
+            return state
+    }
+}
